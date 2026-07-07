f96e77613b55f96dc3fd1e841744a69a4f11fe25
szqq7 <szqq7@qq.com>
2026-07-07 16:46:44 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 	adapter: cloudflare(),
 	experimental: {
     cache: { provider: cloudflareCache() },
-  },
+	},
 	image: {
 		layout: "constrained",
 		responsiveStyles: true,
